af0a7e92ebc66d5cc9459953bb91a9973c104d69
Dylan Schneider <dylan.schneider@perkinelmer.com>
2018-02-22 15:11:32 -0500

    Testing automated build.

--- fileTest.java
+++ fileTest.java
@@ -3,7 +3,7 @@ Public fileTest {
   public static void main String(args []){
     boolean pass = true;
     if (pass) {
-    System.out.println("Hello");
+    System.out.println("Test complete");
     }
     else {
       fail();

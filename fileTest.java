89ce779f7667cf9e5c5bef937eee02506b7fdcd5
Dylan Schneider <dylan.schneider@perkinelmer.com>
2018-02-22 15:18:51 -0500

    Another test.

--- fileTest.java
+++ fileTest.java
@@ -3,7 +3,7 @@ Public fileTest {
   public static void main String(args []){
     boolean pass = true;
     if (pass) {
-    System.out.println("Test completed.");
+    System.out.println("Test completed!");
     }
     else {
       fail();

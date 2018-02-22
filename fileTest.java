138897d8d4181f53e716ca7b0fee8105d09fc061
Dylan Schneider <dylan.schneider@perkinelmer.com>
2018-02-22 14:32:50 -0500

    testing.

--- fileTest.java
+++ fileTest.java
@@ -2,8 +2,9 @@ Public fileTest {
 
   public static void main String(args []){
     boolean pass = true;
-    if (pass)
+    if (pass) {
     System.out.println("Hello");
+    }
     else
       fail();
     }
